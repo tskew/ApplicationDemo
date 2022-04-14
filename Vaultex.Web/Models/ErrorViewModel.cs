80731b1e0d9f60165de6cc16b30d3994b08bac2a
Tim Skew <timskew@gmail.com>
2022-04-14 09:32:02 +0200

    Add project files.

---
+++ Vaultex.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vaultex.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
